2834ebd8be4fd2073292c67abd35eaa17a529ee9
BryanHHZavala <zavalarodriguez88@gmail.com>
2024-09-15 20:47:54 -0600

    --u modificamos app.js

--- app/app.js
+++ app/app.js
@@ -3,24 +3,32 @@
 const express = require('express');
 const cors = require('cors');
 const App = express();
+const bodyParser = require('body-parser');
 //const periodoController = require('./controllers/periodoController');
 
 App.use(cors());
 App.use(express.json());
 App.use(express.urlencoded({ extended: false }));
 
+App.use(bodyParser.json())
+
 //Declaro las rutas
 const clases = require('./routes/clasesRoute');
 const detalle_periodo = require('./routes/detalle_periodoRoute');
 const periodo = require('./routes/periodoRoute');
 const clase_docente = require('./routes/clases_docenteRoute');
-const catedraticos = require('./routes/catedraticosRoute');
-
+const secciones_clase = require('./routes/secciones_claseRoute');
+const ver_catedraticos = require('./routes/catedraticosRoute');
+const ver_periodos = require('./routes/mostrar_periodosRoute');
+const actualizar_detalle = require('./routes/actualizar_detalleRoute');
 
 App.use('/api', clases);
 App.use('/api', detalle_periodo);
 App.use('/api', periodo);
 App.use('/api', clase_docente);
-App.use('/api', catedraticos);
+App.use('/api', secciones_clase);
+App.use('/api', ver_catedraticos);
+App.use('/api', ver_periodos);
+App.use('/api', actualizar_detalle);
 
 module.exports = App;
